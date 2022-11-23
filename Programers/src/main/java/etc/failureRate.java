cdce064b54357cbf19479191a702dc1d7c3fc03a
KANG JIHYO <luxuryjihyo@naver.com>
2022-11-23 17:22:10 +0900

    feat : failureRate

--- Programers/src/main/java/etc/failureRate.java
+++ Programers/src/main/java/etc/failureRate.java
@@ -2,22 +2,25 @@ package etc;
 
 public class failureRate {
     public static void main(String[] args) {
-        int[] stages = new int[]{4,4,4,4,4};
+        int[] stages = new int[]{3,1,1,2,2};
         int N = 4;
         int[] answer = {};
         answer = new int[N];
 
         double[] failureRate = new double[N+2];
         int[] failureNum = new int[N+2];
-        int userNum = stages.length;
-        for(int i : stages){
+
+        int userNum = stages.length; //유저 수
+        for(int i : stages){ // 스테이지 별 멈춰있는 유저 수
             failureNum[i] += 1 ;
         }
-        for(int j = 1; j <= N+1; j++){
+
+        for(int j = 1; j <= N+1; j++){ // 각 스테이지 별 실패율
             failureRate[j] = (double) failureNum[j]/userNum;
             userNum -= failureNum[j];
+            System.out.println(failureRate[j]);
+            if(userNum == 0) break; // 설정 안해주면 ArithmeticException발생 -> 숫자를 0으로 나누려고할 때 발생하는 에러
         }
-
         double max;
         for(int j = 1 ; j < N+1 ; j++){
             max = -0.1;
