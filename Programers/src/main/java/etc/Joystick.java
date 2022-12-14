e77074ca963a90b4f1878fca9773e999878b69be
KANG JIHYO <luxuryjihyo@naver.com>
2022-12-14 14:08:11 +0900

    feat : Joystick

--- Programers/src/main/java/etc/Joystick.java
+++ Programers/src/main/java/etc/Joystick.java
@@ -2,31 +2,38 @@ package etc;
 
 public class Joystick {
     public static void main(String[] args) {
-        String name = "BBAA";
+        String name = "AAABBBABA";
         int answer = 0;
 
-        //위치 카운트
+        //위치(좌우) 카운트
         int cnt = 0;
 
         int len = name.length();
         int current = 0;
-        int min = len-1;
+        int min = 0;
+        //좌우 이동 최대값 설정
+        for(int j =1;j < len ; j++){
+            if(name.charAt(j) !='A'){
+                min = j;
+            }
+        }
 
-        for(int i =0;i < len ; i++){
-            if(name.charAt(i) != 'A') {
-                cnt = i;
-                min = Math.min(min,(len - cnt) + current * 2);
-                min = Math.min(min,(len - cnt) * 2 + current);
+        // 좌우 이동 비교
+        for(int i =1;i < len ; i++){
+
+            if(name.charAt(i) !='A'){
+                min = Math.min(min, (current*2) + len -i);
+                min = Math.min(min, current + (len -i)*2 );
                 current = i;
             }
         }
 
-
-        System.out.println(answer);
-
+        answer = min;
 
 
-        //알파벳 카운트
+
+
+        //알파벳(상하) 카운트
         for(int i = 0; i < len;i++){
             char a = 'A';
             cnt = 0;
