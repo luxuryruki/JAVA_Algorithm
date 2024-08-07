140c39506e68d325d446beefdb12361fccf5a507
KANG JIHYO <luxuryjihyo@naver.com>
2024-08-07 09:28:37 +0900

    add data structure

---
+++ DataStrructure/src/main/java/com/example/datastrructure/DataStrructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.datastrructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStrructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStrructureApplication.class, args);
+    }
+
+}
---
+++ DataStrructure/src/test/java/com/example/datastrructure/DataStrructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datastrructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStrructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
