305fa36315951bbaf5c140f04060c27a36cac596
KANG JIHYO <luxuryjihyo@naver.com>
2024-08-09 17:39:31 +0900

    add SinglyLinkedList

--- DataStrructure/src/main/java/com/example/datastrructure/linkedlist/SinglyLinkedList.java
+++ DataStrructure/src/main/java/com/example/datastrructure/linkedlist/SinglyLinkedList.java
@@ -1,11 +1,8 @@
 package com.example.datastrructure.linkedlist;
 
-import com.example.datastrructure.DataStrructureApplication;
-import org.springframework.boot.SpringApplication;
-
 public class SinglyLinkedList {
     public static void main(String[] args) {
-        Node head = new Node(1);
+        SinglyNode head = new SinglyNode(1);
         head.append(2);
         head.append(3);
         head.append(4);
