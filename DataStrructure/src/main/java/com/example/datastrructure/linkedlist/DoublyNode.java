880e0d8edc83c973d507f4655db1c06641d9217b
KANG JIHYO <luxuryjihyo@naver.com>
2024-08-12 18:41:48 +0900

    add DoublyNode

---
+++ DataStrructure/src/main/java/com/example/datastrructure/linkedlist/DoublyNode.java
@@ -0,0 +1,26 @@
+package com.example.datastrructure.linkedlist;
+
+public class DoublyNode implements Node{
+    int value;
+    DoublyNode previous;
+    DoublyNode next;
+
+    public DoublyNode(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public void append(int a) {
+
+    }
+
+    @Override
+    public void delete(int d) {
+
+    }
+
+    @Override
+    public void retrieve() {
+
+    }
+}
