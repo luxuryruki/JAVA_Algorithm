b921b08a6c46855411abfd44cdb8540f8b544fa4
gangjihyo <luxuryjihyo@naver.com>
2025-01-28 12:31:55 +0900

    주석 삽입

--- DataStrructure/src/main/java/com/example/datastrructure/hashtable/HashTable.java
+++ DataStrructure/src/main/java/com/example/datastrructure/hashtable/HashTable.java
@@ -3,6 +3,20 @@ package com.example.datastrructure.hashtable;
 
 import java.util.LinkedList;
 
+/*
+* 1. 키와 값을 담을 노드 생성
+*   - Key & Value 선언
+*   - Constructor Getter Setter 생성
+* 2. 해시테이블 생성
+*   - Node 담을 LinkedList 선언
+*   - Linkedlist 사이즈를 받아서 해시테이블 constructor 선언
+* 3. 해시테이블 기능정의
+*   - getHashCode : 해시값 생성
+*   - convertToIndex : 해시값을 이용해 인덱스 생성
+*   - put : 키밸류 삽입
+*   - get : 밸류 확인
+*
+* */
 public class HashTable {
     LinkedList<Node>[] data;
 
